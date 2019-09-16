0a680b913f4ec1b01e1436cd61bf5a7a857c0554
yingjieweb <1184061078@qq.com>
2019-09-16 16:11:03 +0800

    '封装拦截器'

--- src/network/request.js
+++ src/network/request.js
@@ -1,66 +1,6 @@
 import axios from 'axios'
 
-//封装方式一
-/*export function  request(config,success,failure) {
-    //1.创建axios实例
-    const instance = axios.create({
-        baseURL:'http://123.207.32.32:8000',
-        timeout:5000
-    })
-
-    //发送真正的网络请求
-    instance(config)
-        .then(res => {
-            //console.log(res);
-            success(res);
-        })
-        .catch(err => {
-            //console.log(err);
-            failure(err);
-        })
-}*/
-
-//封装方式二
-/*export function  request(config) {
-    //1.创建axios实例
-    const instance = axios.create({
-        baseURL:'http://123.207.32.32:8000',
-        timeout:5000
-    })
-
-    //发送真正的网络请求
-    instance(config.baseConfig)
-        .then(res => {
-            //console.log(res);
-            config.success(res);
-        })
-        .catch(err => {
-            //console.log(err);
-            config.failure(err);
-        })
-}*/
-
-//封装方式三:
-/*export function  request(config) {
-    return new Promise((resolve,reject) => {
-        //1.创建axios实例
-        const instance = axios.create({
-            baseURL:'http://123.207.32.32:8000',
-            timeout:5000
-        })
-
-        //发送真正的网络请求
-        instance(config)
-            .then(res => {
-                resolve(res)
-            })
-            .catch(err => {
-                reject(err)
-            })
-    })
-}*/
-
-//封装方式四
+//封装
 export function  request(config) {
     //1.创建axios实例
     const instance = axios.create({
