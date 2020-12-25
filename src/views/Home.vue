b778f31557750d0af7f078fa77ea948dcaa1a511
yingjieweb <1184061078@qq.com>
2020-12-25 11:30:12 +0800

    log test signature url

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -13,7 +13,7 @@
     methods: {
       async scan() {
         let url = location.href.split("#")[0]
-        const {data} = await getSign(url)
+        const {data} = await getSign(url) //http://yingjiesongi.gitee.io/gate-code/
         wx.config({
           debug: false,// 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
           appId: data.appId,// 必填，公众号的唯一标识
